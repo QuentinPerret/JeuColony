df270c74a7118848fc03d8b56911e42df689cb10
Quentin Perret <quentinprrt33@gmail.com>
2022-03-14 10:57:22 +0100

    Ajout lvl

--- JeuColony/PNJ/AllyFolder/Profession/Builder.cs
+++ JeuColony/PNJ/AllyFolder/Profession/Builder.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace JeuColony.PNJ.AllyFolder
 {
-    class Builder : Ally, IAllyCalculus
+    class Builder : Ally
     {
         public Builder(string name, int healthpointmax, int healthpoint, int attackpower, int visionrange, int[] coordinate, int diggerpower, int buildingpower, int loggingpower) : base(name, healthpointmax, healthpoint, attackpower, visionrange, coordinate, diggerpower, buildingpower, loggingpower) { }
         
